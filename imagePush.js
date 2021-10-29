8fb3d904b26264e1fdd185b21b6ce8f6496ab0b7
JMekker <jmekker@gmail.com>
2021-10-28 19:17:22 -0700

    attempting to fix things

--- imagePush.js
+++ imagePush.js
@@ -4,17 +4,22 @@ const fs = require('fs');
 const directoryPath = path.join(__dirname, './images/art');
 const links = [];
 
-fs.readdir(directoryPath, function (err, files) {
-	if (err) {
-		return console.log('Unable to scan directory: ' + err);
-	}
-	files.forEach(function (file) {
-		links.push(file)
-		console.log(file);
+function readFiles() {
+	fs.readdir(directoryPath, function (err, files) {
+		if (err) {
+			return console.log('Unable to scan directory: ' + err);
+		}
+		files.forEach(function (file) {
+			links.push(file)
+			console.log(file);
+		});
+		console.log(links);
 	});
-	console.log(links);
-});
+}
 
-for (link of links) {
-	document.getElementById('images').innerHTML += `<a href=${link}><img src=${link} width=120 /></a>';
+window.onload = function () {
+	readFiles();
+	links.forEach(link => {
+		document.getElementById('images').innerHTML += `<a href=${link}><img src=${link} width=120 /></a>`;
+	});
 }
